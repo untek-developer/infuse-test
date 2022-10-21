c13697e0a7de1a068c10edc936f831cc3615f730
vitaliy <zndev@ya.ru>
2022-10-21 16:38:25 +0600

    feat: make service

--- public/banner.php
+++ public/banner.php
@@ -4,3 +4,5 @@ $bannerFile = __DIR__ . '/images/banner.jpg';
 header('Content-Type: image/jpeg');
 readfile($bannerFile);
 
+$bannerCounterService = new \App\Services\BannerCounterService();
+$bannerCounterService->increment();
---
+++ src/Services/BannerCounterService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Services;
+
+class BannerCounterService
+{
+
+    public function increment(): void {
+        
+    }
+}
